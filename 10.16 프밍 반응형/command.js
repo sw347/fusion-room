b9459571d1efbd1f7cb386cab1bc7d2c79ce7e3b
jiyoonyee <136244048+jiyoonyee@users.noreply.github.com>
2023-12-01 17:44:46 +0900

    Add files via upload

---
+++ 10.16 프밍 반응형/command.js
@@ -0,0 +1,7 @@
+// const menuBars = document.getElementById("menu_bars");
+// const menuClose = document.querySelector(".mobile_close");
+// const nav = document.getElementsByClassName("nav");
+
+// menuClose.addEventListener("click",function(){
+//     nav.style.margin = "70vw";
+// });
