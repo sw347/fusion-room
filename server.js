40c8b8c5e891e5f765dc1e65cb96e530a12cc06e
sw347 <sdh230310@sdh.hs.kr>
2023-12-12 09:09:41 +0900

    server
    Ctrl + j를 눌러 터미널을 키고,
    npm init을 치고
    npm i를 친 후 설치가 다 되면
    npm start를 쳐서 서버 실행
    Live server키지마

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express"); //express모듈 가져오기
 const nunjucks = require("nunjucks"); // nunjucks 모듈 가져오기(html 파일을 만들기 위한 모듈)
 const app = express(); //app 객체를 이용해 express와 관련된 함수를 app.method()를 이용해 사용 가능하게 함
+const path = require("path");
+const port = 8000;
 
-app.listen('8080',()=>{
-    console.log("8080포트로 서버 연결");
-});//8080포트로 연결
-
-app.get('/get',(req,res)=>{
-    res.send("ㅎㄷㄷ")
+nunjucks.configure(path.join(__dirname, "room", "ui"), {
+  autoescape: true,
+  express: app,
 });
+
+app.use(express.static(path.join(__dirname, "room")));
+app.use(express.static(path.join(__dirname, "room", "ui")));
+app.use(express.static(path.join(__dirname, "room", "style")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "room", "ui", "main.html"));
+});
+
+app.listen(port, () => {
+  console.log(`서버가 실행됩니다. http://localhost:${port}`);
+}); //8080포트로 연결
